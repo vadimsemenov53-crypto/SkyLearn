b812c92b1c50dbaf3978de15256fe79547ca099a
Vadim Semenov <vadimsemenov53@gmail.com>
2026-07-18 20:43:39 +0300

    serialization field restriction

--- users/serializers.py
+++ users/serializers.py
@@ -6,4 +6,4 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ("id", "email", "first_name", "last_name", "phone", "city",)
