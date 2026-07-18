01672025059964a958eb7ec2e3f6530337c51a91
Vadim Semenov <vadimsemenov53@gmail.com>
2026-07-18 17:33:06 +0300

    add all views for CRUD -> Course(ViewSet) & Lesson (generics)

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    UpdateAPIView,
+    RetrieveAPIView,
+    DestroyAPIView
 
-# Create your views here.
+)
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonCreateAPIView(CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
