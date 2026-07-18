ce2f9de4ef6122c01e5656b657db0a7a270f8268
Vadim Semenov <vadimsemenov53@gmail.com>
2026-07-18 20:10:27 +0300

    add urls for all views Course and Lesson

---
+++ materials/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+from materials.apps import MaterialsConfig
+from materials.views import CourseViewSet, LessonListAPIView, LessonCreateAPIView, LessonRetrieveAPIView,LessonUpdateAPIView, LessonDestroyAPIView
+
+app_name = MaterialsConfig.name
+
+router = SimpleRouter()
+router.register(r"course", CourseViewSet)
+
+urlpatterns = [
+    path('lessons/', LessonListAPIView.as_view(), name='lessons_list'),
+    path('lessons/create/', LessonCreateAPIView.as_view(), name='lessons_create'),
+    path('lessons/<int:pk>', LessonRetrieveAPIView.as_view(), name='lessons_retrieve'),
+    path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lessons_update'),
+    path('lessons/<int:pk>/delete', LessonDestroyAPIView.as_view(), name='lessons_delete'),
+]
+
+urlpatterns += router.urls
--- materials/views.py
+++ materials/views.py
@@ -11,31 +11,37 @@ from rest_framework.generics import (
 )
 
 class CourseViewSet(ModelViewSet):
+    """ Контроллер для модели Course использующий ModelViewSet """
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
 class LessonCreateAPIView(CreateAPIView):
+    """ Контроллер создания единицы модели Lesson. """
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
 
 class LessonListAPIView(ListAPIView):
+    """ Контроллер отображения всех единиц модели Lesson. """
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
+    """ Контроллер отображения единицы модели Lesson. """
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
 
 class LessonUpdateAPIView(UpdateAPIView):
+    """ Контроллер редактирования единицы модели Lesson. """
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
 
 class LessonDestroyAPIView(DestroyAPIView):
+    """ Контроллер удаления единицы модели Lesson. """
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
