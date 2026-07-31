632f7bc765f2b16f81592a32bffab970426d3e5b
Vadim Semenov <vadimsemenov53@gmail.com>
2026-07-31 18:33:23 +0300

    add validator YouTubeValidateVideoURL

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from materials.models import Course, Lesson
+from materials.validators import YouTubeValidateVideoURL
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    video_url = serializers.URLField(validators=[YouTubeValidateVideoURL()])
+
     class Meta:
         model = Lesson
         fields = "__all__"
