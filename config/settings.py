7caaaaf0069b19f600a363381d584884f43ca403
Vadim Semenov <vadimsemenov53@gmail.com>
2026-08-23 21:29:59 +0300

    add two files Dockerfile and docker-compose.yml (tests good work!), rebuild .env.sample + .dockerignore;

--- config/settings.py
+++ config/settings.py
@@ -157,7 +157,7 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 
-CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabasesScheduler"
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 CELERY_BEAT_SCHEDULE = {
     "send_information_about_update_periodical": {
